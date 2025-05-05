327d75df17d8ae4d06533130695605bf4364cbbf
ASH <144093084+ashutoshh-17@users.noreply.github.com>
2025-05-05 12:47:13 +0530

    Pushhh

---
+++ docker/install-node.sh
@@ -0,0 +1,11 @@
+# Download and install fnm:
+winget install Schniz.fnm
+
+# Download and install Node.js:
+fnm install 22
+
+# Verify the Node.js version:
+node -v # Should print "v22.15.0".
+
+# Verify npm version:
+npm -v # Should print "10.9.2".
